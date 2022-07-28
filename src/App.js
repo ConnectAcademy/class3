e677b8ed5281c22c021598894a2c2fad8ab3800e
Admen-UltraSoft <admenn94@gmail.com>
2022-07-28 16:43:34 +0200

    some basics about react state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState("")
+
+  const increment = () => {
+    setCount((prev) => prev + 1)
+  }
+
+  const decrement = () => {
+    setCount(prev => prev - 1)
+  }
+
+  const handleSubmit = e => {
+    setUserInput(e.target.value)
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <>
+    <h1>{count}</h1>
+    <h1>{userInput}</h1>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>+</button>
+    <input onChange={handleSubmit}/>
+    </>
   )
 }
 
