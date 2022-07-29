7e9f5ff63d14823a41e6d94dac69fef1eb92d317
Admen-UltraSoft <admenn94@gmail.com>
2022-07-29 14:40:45 +0200

    some state exercise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
-}
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [age, setAge] = useState(null);
 
-export default App
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(name, surname, age);
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{surname}</h3>
+      <h3>{age}</h3>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setName(e.target.value)} />
+        <input onChange={(e) => setSurname(e.target.value)} />
+        <input onChange={(e) => setAge(e.target.value)} />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default App;
